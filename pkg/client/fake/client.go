557dd1673ab7dcbddeb956cd9c3d49762f625b9b
Marko Lukša <marko.luksa@gmail.com>
2019-10-29 13:32:15 +0100

    Fake client's List() should allow nil ListOptions like the real client

--- pkg/client/fake/client.go
+++ pkg/client/fake/client.go
@@ -101,8 +101,13 @@ func (c *fakeClient) List(ctx context.Context, opts *client.ListOptions, list ru
 		gvk = opts.Raw.TypeMeta.GroupVersionKind()
 	}
 
+	ns := ""
+	if opts != nil {
+		ns = opts.Namespace
+	}
+
 	gvr, _ := meta.UnsafeGuessKindToResource(gvk)
-	o, err := c.tracker.List(gvr, gvk, opts.Namespace)
+	o, err := c.tracker.List(gvr, gvk, ns)
 	if err != nil {
 		return err
 	}
@@ -116,7 +121,7 @@ func (c *fakeClient) List(ctx context.Context, opts *client.ListOptions, list ru
 		return err
 	}
 
-	if opts.LabelSelector != nil {
+	if opts != nil && opts.LabelSelector != nil {
 		return filterListItems(list, opts.LabelSelector)
 	}
 	return nil
